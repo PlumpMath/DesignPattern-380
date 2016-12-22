7e8307ec35e95a308d566057346a87d9a0893848
xiaozhaowen <hblg703@63.com>
2016-12-22 22:22:30 +0800

    Initial commit

---
+++ src/com/xiaozhao/observer/IObserver.java
@@ -0,0 +1,9 @@
+package com.xiaozhao.observer;
+
+/**
+ * Created by xiaozhao on 2016/12/22.
+ */
+public interface IObserver {
+
+    void doAction(String stateInfo);
+}
---
+++ src/com/xiaozhao/observer/Subject.java
@@ -0,0 +1,8 @@
+package com.xiaozhao.observer;
+
+/**
+ * Created by xiaozhao on 2016/12/22.
+ */
+public abstract class Subject {
+
+}
